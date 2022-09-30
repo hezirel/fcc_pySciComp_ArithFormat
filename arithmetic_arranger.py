b4dbca059ecd15fd095f164443338666fdd23141
hezirel <80560775+hezirel@users.noreply.github.com>
2022-09-30 09:24:19 +0200

    Switch for error handling

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,15 @@
-def arithmetic_arranger(problems):
+class TooManyProblems(Exception):
+    pass
 
+def arithmetic_arranger(problems, ans=False):
 
+    try:
+        if len(problems) > 5:
+            raise TooManyProblems
+    except TooManyProblems:
+        return ("Error: Too many problems.")
+
+    arranged_problems = "Solution to come"
     return arranged_problems
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True));
